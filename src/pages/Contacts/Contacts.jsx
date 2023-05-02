8595524396464b564eb0fc485ad8450b809f509c
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-02 22:49:00 +0300

    Create Contacts.jsx

---
+++ src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Section } from 'components/Section/Section';
+import { AddContactsForm } from 'components/AddContactsForm/AddContactsForm';
+import { ContactsList } from 'components/ContactsList/ContactsList';
+import { Filter } from 'components/Filter/Filter';
+import { Wrapper } from 'components/App/App.styled';
+import Loader from 'components/Loader/Loader';
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectError, selectIsLoading } from 'redux/contacts/selectors';
+
+const Contacts = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <Wrapper>
+      <Section title="Phonebook">
+        <AddContactsForm />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <ContactsList />
+        {isLoading && !error && <Loader />}
+        {!isLoading && error && <p>Sorry, but something go wrong...</p>}
+      </Section>
+    </Wrapper>
+  );
+};
+
+export default Contacts;
