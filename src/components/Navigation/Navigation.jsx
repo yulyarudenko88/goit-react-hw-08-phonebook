5ef592e50d30a0043195ce38e6a0bc1087fba133
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-02 22:48:29 +0300

    Create Navigation.jsx

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from 'react-router-dom';
+
+export const Navigation = ()=>{
+  return(<nav>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/contacts">Contacts</NavLink>
+    <NavLink to="/register">Register</NavLink>
+    <NavLink to="/login">Login</NavLink>
+  </nav>)
+}
