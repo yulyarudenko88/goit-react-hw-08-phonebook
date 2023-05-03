51ae36b6f62d197f3c8497483d13a88feb630fe8
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-03 18:57:20 +0300

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,32 +1,47 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://64417f1efadc69b8e085a7b1.mockapi.io/contacts';
+// axios.defaults.baseURL = 'https://64417f1efadc69b8e085a7b1.mockapi.io/contacts';
 
+/*
+ * Get all user contacts
+ * GET @ /contacts
+ * headers: Authorization: Bearer token
+ */
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('/contacts');
-      return response.data;
+      const res = await axios.get('/contacts');
+      return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
+/*
+ * Create a new contact
+ * GET @ /contacts
+ * body: { name, number }
+ */
 export const addContact = createAsyncThunk(
   'contacts/addContact',
-  async (data, thunkAPI) => {
+  async ({ name, number }, thunkAPI) => {
     try {
-      const response = await axios.post('/contacts', { ...data });
-      return response.data;
+      const res = await axios.post('/contacts', { name, number });
+      return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
+/*
+ * Delete contact
+ * GET @ /contacts/{contactId}
+ * body: { contactId }
+ */
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (contactId, thunkAPI) => {
