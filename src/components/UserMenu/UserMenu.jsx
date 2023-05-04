50b2e4482494781562f278d6393b7cb651528a20
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-04 14:35:01 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,17 +1,17 @@
-// import { useDispatch } from 'react-redux';
-// import { logOut } from 'redux/auth/operations';
-// import { useAuth } from 'hooks';
+import { useDispatch } from 'react-redux';
+import { logOut } from 'redux/auth/operations';
+import { useAuth } from 'hooks/useAuth';
 
 export const UserMenu = () => {
-  // const dispatch = useDispatch();
-  // const { user } = useAuth();
+  const dispatch = useDispatch();
+  const { user } = useAuth();
 
   return (
     <div>
-      <p>Welcome</p>
-      {/* <button type="button" onClick={() => dispatch(logOut())}>
+      <p>Welcome, {user.email}</p>
+      <button type="button" onClick={() => dispatch(logOut())}>
         Logout
-      </button> */}
+      </button>
     </div>
   );
 };
