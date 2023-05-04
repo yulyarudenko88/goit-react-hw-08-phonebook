036afb4d180a6036c2e2c6e7b9b135acfb3e1c04
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-04 14:34:18 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { lazy, useEffect } from 'react';
 
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
 import { Layout } from 'components/Layout/Layout';
-import Home from 'pages/Home/Home';
-import Contacts from 'pages/Contacts/Contacts';
-import Register from 'pages/Register/Register';
-import Login from 'pages/Login/Login';
+import { useAuth } from 'hooks/useAuth';
+import { refreshUser } from 'redux/auth/operations';
+import Loader from 'components/Loader/Loader';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Register = lazy(() => import('pages/Register/Register'));
+const Login = lazy(() => import('pages/Login/Login'));
+const Contacts = lazy(() => import('pages/Contacts/Contacts'));
 
 export const App = () => {
-  return (
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <Loader />
+  ) : (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-        <Route index element={<Home/>} />
-          <Route path="contacts" element={<Contacts/>} />
-          <Route path="register" element={<Register/>} />
-          <Route path="login" element={<Login/>} />
+          <Route index element={<Home />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
           {/* <Route path="*" element={<NotFound />} /> */}
         </Route>
       </Routes>
-      
     </>
   );
 };
