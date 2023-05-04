a67d3585f24575d333a38af2e7c2590cd29ad30f
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-04 14:34:35 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,7 +4,5 @@ export const Navigation = ()=>{
   return(<nav>
     <NavLink to="/">Home</NavLink>
     <NavLink to="/contacts">Contacts</NavLink>
-    <NavLink to="/register">Register</NavLink>
-    <NavLink to="/login">Login</NavLink>
   </nav>)
 }
