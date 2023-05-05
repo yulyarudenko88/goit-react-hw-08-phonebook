b870defd5e0af2a52ab58e6aa1bc55eee6f8b84a
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-06 02:06:04 +0300

    UPDATE: submit button styles

--- src/components/AddContactsForm/AddContactsForm.styled.js
+++ src/components/AddContactsForm/AddContactsForm.styled.js
@@ -35,3 +35,18 @@ export const Input = styled.input`
     border: 1px solid #6a0dad;
   }
 `;
+
+export const Btn = styled.button`
+  display: flex;
+  background-color: #b19cd9;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    background-color: #9376b0;
+  }
+`;
