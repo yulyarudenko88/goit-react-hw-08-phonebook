e45616bd3a5831d4cea5dfa47e5cb0db34b5c9b7
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-06 02:13:03 +0300

    UPDATE:  styling elements using mui

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,45 +2,23 @@ import { Navigation } from 'components/Navigation/Navigation';
 import { UserMenu } from 'components/UserMenu/UserMenu';
 import { AuthNav } from 'components/AuthNav/AuthNav';
 import { useAuth } from 'hooks/useAuth';
-import { AppBar as MyAppBar } from '@material-ui/core';
-
+import { AppBar as MyAppBar } from '@mui/material';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <MyAppBar position="static" sx={{ bgcolor: '#8a2be2' }}>
+    <MyAppBar
+      position="static"
+      sx={{
+        bgcolor: '#b19cd9',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}
+    >
       <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </MyAppBar>
   );
 };
-// import React from 'react';
-// import { AppBar as MuiAppBar, Toolbar, Button, Typography } from '@material-ui/core';
-// import { NavLink } from 'react-router-dom';
-// import { useAuth } from 'hooks/useAuth';
-
-// export const AppBar = () => {
-//   const { isLoggedIn } = useAuth();
-
-//   return (
-//     <MuiAppBar >
-//       <Toolbar>
-//         <IconButton edge="start" color="inherit">
-          // <Book />
-          // </IconButton>
-//         <Button color="inherit" component={NavLink} to="/" exact sx={{ color: '#ffffff', '&:hover': { color: '#6a0dad' }, '&:focus': { color: '#6a0dad' }, '&.active': { color: '#6a0dad' } }}>
-//           Home
-//         </Button>
-//         <Button color="inherit" component={NavLink} to="/contacts" sx={{ color: '#ffffff', '&:hover': { color: '#6a0dad' }, '&:focus': { color: '#6a0dad' }, '&.active': { color: '#6a0dad' } }}>
-//           Contacts
-//         </Button>
-//         {isLoggedIn ? (
-//           
-//         ) : (
-//           
-//         )}
-//       </Toolbar>
-//     </MuiAppBar>
-//   );
-// };
