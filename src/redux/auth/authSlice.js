553407e6f7cf99404b6b52bdabe6a1432614781e
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-06 02:23:00 +0300

    Update authSlice.js

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,14 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { register, logIn, logOut, refreshUser } from './operations';
-import { handlePending, handleRejected } from './handleRequests';
 
 const initialState = {
   user: { name: null, email: null },
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
-  isLoading: false,
-  error: null,
 };
 
 const authSlice = createSlice({
@@ -16,27 +13,21 @@ const authSlice = createSlice({
   initialState,
   extraReducers: builder => {
     builder
-      .addCase(register.pending, handlePending)
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(register.rejected, handleRejected)
-      .addCase(logIn.pending, handlePending)
       .addCase(logIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(logIn.rejected, handleRejected)
-      .addCase(logOut.pending, handlePending)
       .addCase(logOut.fulfilled, (state, action) => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
       })
-      .addCase(logOut.rejected, handleRejected)
       .addCase(refreshUser.pending, state => {
         state.isRefreshing = true;
       })
