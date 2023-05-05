8f886794ea42126c196a87dac43c1adbdc50df34
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-05 13:20:10 +0300

    UPDATE Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 const Home = () => {
-  return(<p>Home</p>)
-}
+  return (
+    <div>
+      <h1>Телефонна книга</h1>
+      <p>
+        Універсальний додаток для роботи з вашими контактами, який пропонує
+        можливість їх створення, зберігання та видалення.
+      </p>
+      <p>
+        Якщо ви ще не зареєстровані, перейдіть за посиланням
+        <NavLink to="/register">Register</NavLink>
+      </p>
+      <p>
+        Якщо ви вже зарєєстровані, вам потрібно залогінитись
+        <NavLink to="/login">Log In</NavLink>
+      </p>
+      <p>Насолоджуйтесь!</p>
+    </div>
+  );
+};
 
 export default Home;
