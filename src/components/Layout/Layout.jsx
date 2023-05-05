edbfc97cdd2d26d935c40a036b5e80f25a967998
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-06 02:15:44 +0300

    UPDATE: add react-toastify

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,19 @@
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { AppBar } from 'components/AppBar/AppBar';
 import { Suspense } from 'react';
-
+import Loader from 'components/Loader/Loader';
 
 export const Layout = () => {
   return (
     <>
+      <ToastContainer autoClose={3000} />
       <AppBar />
-      <Suspense fallback={null}>
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
-      {/* <Toaster position="top-right" reverseOrder={false} /> */}
     </>
   );
 };
