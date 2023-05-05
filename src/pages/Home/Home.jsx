9829273df1166fcc4742d9fe4733713b369f3b3c
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-06 02:22:35 +0300

    UPDATE: translate into english

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,24 @@
+import { Container } from '@mui/material';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Description, Btn } from './Home.styled';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Телефонна книга</h1>
-      <p>
-        Універсальний додаток для роботи з вашими контактами, який пропонує
-        можливість їх створення, зберігання та видалення.
-      </p>
-      <p>
-        Якщо ви ще не зареєстровані, перейдіть за посиланням
-        <NavLink to="/register">Register</NavLink>
-      </p>
-      <p>
-        Якщо ви вже зарєєстровані, вам потрібно залогінитись
-        <NavLink to="/login">Log In</NavLink>
-      </p>
-      <p>Насолоджуйтесь!</p>
-    </div>
+    <Container maxWidth="sm">
+      <h1>Phonebook</h1>
+      <Description>
+        A versatile application for working with your contacts, which offers the
+        ability to create, store and delete them.
+      </Description>
+      <Description>
+        If you are not registered yet, follow the link
+        <Btn to="/register">Register</Btn>
+      </Description>
+      <Description>
+        If you are registered, go to the authorization page
+        <Btn to="/login">Log In</Btn>
+      </Description>
+    </Container>
   );
 };
 
